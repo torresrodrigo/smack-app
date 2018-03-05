c9e22ef6843b4612bd5900eac0721c6fc98bcd34
Rodrigo <b.rodrigotorres@gmail.com>
2018-03-05 02:06:05 -0300

    Channel VC Ready

---
+++ Smack-App/Smack-App/View/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Smack-App
+//
+//  Created by Rodrigo Torres on 04/03/18.
+//  Copyright © 2018 Rodrigo Torres. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
